9e1e6e25978ebe85f57964d0ce3f164860f00644
Josef Melcr <melcrjos@fit.cvut.cz>
2024-02-19 21:19:03 +0100

    add help message

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-#include "FileTokenizer.h"
-#include "StdinTokenizer.h"
-#include "Parser.h"
 #include "Compiler.h"
+#include "FileTokenizer.h"
+#include "Parser.h"
 #include "SECD.h"
-#include <iostream>
+#include "StdinTokenizer.h"
 #include <cstring>
+#include <iostream>
+
+void print_help() {
+    std::cout << "TinyLisp" << std::endl;
+    std::cout << "-------------------------------------------" << std::endl;
+    std::cout << "Usage: \n\tTinyLisp <filename> to execute a file." << std::endl;
+    std::cout << "\tRun without arguments to use a simple REPL environment."
+              << std::endl;
+}
 
 std::optional<Error> run(const FileTokenizer & t) {
     auto tokens = t.tokenize();
@@ -64,14 +72,14 @@ std::optional<Error> run(const StdinTokenizer & t) {
 int main(int argc, const char ** argv) {
     if (argc == 1) {
         StdinTokenizer t;
-        while (!std::cin.eof()){
+        while (!std::cin.eof()) {
             if (auto result = run(t); result.has_value()) {
                 std::cout << result.value() << std::endl;
             }
         }
     } else if (argc == 2) {
         if (!strcmp(argv[1], "-h")) {
-            std::cout << "help" << std::endl; // TODO
+            print_help();
             return 0;
         } else {
             FileTokenizer t(argv[1]);
@@ -80,6 +88,9 @@ int main(int argc, const char ** argv) {
                 return 1;
             }
         }
+    } else {
+        std::cout << "Invalid number of arguments." << std::endl;
+        return 1;
     }
     return 0;
 }
